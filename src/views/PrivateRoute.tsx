d3f7b921cee533c2bd4f638e93a627b2f08a4470
WesleyEmanuel <wesley.emanuel@prolins.com.br>
2023-01-19 17:45:11 -0300

    tela Artigos com editor de texto quilljs

--- src/views/PrivateRoute.tsx
+++ src/views/PrivateRoute.tsx
@@ -4,7 +4,6 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
   const loggedUser = useSelector(useLoggedUser);
-  console.log(loggedUser.id);
 
   return loggedUser.id ? children : <Navigate to="/admin/login" />;
 };
